b16e8f44f93d6e6b28a9ac61c067bd6c23184be7
Dexter Friedman <Dexter@Dexters-MacBook-Pro-117.local>
2014-01-25 14:44:36 -0800

    before the merge

--- Assets/BasicMovementScript.cs
+++ Assets/BasicMovementScript.cs
@@ -4,76 +4,62 @@ using System.Collections;
 public class BasicMovementScript : MonoBehaviour {
 
 	public float moveSpeed = 3.0f;
-	public float jumpForce = 1;
-
-	private bool grounded = true;
-	private bool falling = false;
-
-	// Use this for initialization
-	void Start () {
+	public float moveForceMultiplier = 9.0f;
+	public float jumpForce = 300.0f;
+	public GameObject myJumpTrigger;
 	
+	private JumpTriggerBehaviour jumpTrig;
+
+	// Start
+	void Start() {
+
+		jumpTrig = myJumpTrigger.GetComponent<JumpTriggerBehaviour>();
+
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	   
+
+	// Jump
+	void Jump() {
+
+		Vector3 jumpVect = transform.TransformDirection ( rigidbody.transform.up ) * jumpForce;
+		rigidbody.velocity = ( jumpVect ); // TODO: Somehow get rigidbody.AddForce() to work            
+
 	}
 
 	// FixedUpdate called once per physics engine update
 	void FixedUpdate() {
 
 		// Rotate camera
-		if (grounded) {
-			if (Input.GetAxis ("Horizontal") != 0) {
-				// Rotate around the z-axis to turn camera view
-				rigidbody.transform.Rotate (new Vector3 (0, moveSpeed * Input.GetAxis ("Horizontal"), 0)); // lies
 
-			}
+		if (Input.GetAxis ("Horizontal") != 0) {
+			// Rotate around the z-axis to turn camera view
+			rigidbody.transform.Rotate (new Vector3 (0, moveSpeed * Input.GetAxis ("Horizontal"), 0)); // lies
+		}
+			
+		// Move forward/backward
+		if (Input.GetAxis ("Vertical") > 0) {
+		
+			rigidbody.AddForce( new Vector3(moveForceMultiplier * rigidbody.transform.forward.x, 
+		      	                           	moveForceMultiplier * rigidbody.transform.forward.y,
+		      	                           	moveForceMultiplier * rigidbody.transform.forward.z));
+		} 
+		else if (Input.GetAxis ("Vertical") < 0) {
 
-			if (Input.GetAxis ("Vertical") > 0) {
-				rigidbody.velocity = new Vector3 (moveSpeed * rigidbody.transform.forward.x, 
-			                                  moveSpeed * rigidbody.transform.forward.y,
-			                                  moveSpeed * rigidbody.transform.forward.z);
-			} 
-			else if (Input.GetAxis ("Vertical") < 0) {
-				rigidbody.velocity = new Vector3 (-moveSpeed * rigidbody.transform.forward.x, 
-			                                  -moveSpeed * rigidbody.transform.forward.y,
-			                                  -moveSpeed * rigidbody.transform.forward.z);
-			}
-				
+			rigidbody.AddForce( new Vector3 ( -moveForceMultiplier * rigidbody.transform.forward.x, 
+		                                 	  -moveForceMultiplier * rigidbody.transform.forward.y,
+		                                 	  -moveForceMultiplier * rigidbody.transform.forward.z));
 		}
 
-		if (Input.GetKey (KeyCode.Space) && grounded) {
+		// Jump
+
+		if ( Input.GetKey(KeyCode.Space) && jumpTrig.canJump() ) {
 			Jump();
-		}
-
-		if (rigidbody.velocity.y < 0) {
-			falling = true;
-		}
-
-		if(falling == true && rigidbody.velocity.y == 0)
-		{
-			grounded = true;
-			falling = false;
 		}
 
 		// Not a problem now, but may be soon. Prevents us from infinitely accelerating past moveSpeed
 		rigidbody.velocity = Vector3.ClampMagnitude( rigidbody.velocity, moveSpeed );
+		myJumpTrigger.transform.Translate( rigidbody.velocity );  // TODO: trigger collider isn't moving with us
 
 	}
 
-	void Jump(){
-		grounded = false;
 
-		rigidbody.velocity = new Vector3 (jumpForce * rigidbody.transform.up.x, 
-		                                  jumpForce * rigidbody.transform.up.y, 
-		                                  jumpForce * rigidbody.transform.up.z);
-
-
-
-
-	
-
-
-	}
 }
