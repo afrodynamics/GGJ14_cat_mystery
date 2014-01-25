c3a8355480dbd0790fba9735d91c27c4d3263462
Dexter Friedman <Dexter@Dexters-MacBook-Pro-117.local>
2014-01-25 14:45:45 -0800

    before the merge

---
+++ Assets/JumpTriggerBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JumpTriggerBehaviour : MonoBehaviour {
+	
+	public bool grounded = true;
+
+	// If we have a collision with the terrain, set grounded to true
+	
+	void OnCollisionEnter ( Collision coll ) {
+		grounded = true;
+		Debug.Log("collision entered: ground = " + ( grounded ) );
+		// NOTE: This will NOT work for generic GameObjects!
+	}
+	
+	void OnCollisionExit ( Collision coll ) {
+		grounded = false;
+		Debug.Log("collision exited: ground = " +  ( grounded ));
+	}
+
+	public bool canJump() {
+		return grounded;
+	}
+}
