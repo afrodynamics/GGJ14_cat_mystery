0106f06a92e5ccb52ba9ad98cf10869ffd1cc788
dqiri <elc036@ucsd.edu>
2014-01-25 19:00:10 -0800

    Paddle controls
    basic paddle controls

--- Assets/PaddleControl.cs
+++ Assets/PaddleControl.cs
@@ -12,7 +12,15 @@ public class PaddleControl : MonoBehaviour {
 	}
 
 	void LeapControls() {
+		Frame frame = m_leapController.Frame();
 
+		if (frame.Hands.Count == 1) {
+
+			Hand hand = frame.Hands[0];
+
+			transform.forward = new Vector3 ( hand.PalmNormal.x, hand.PalmNormal.y, hand.PalmNormal.z);
+
+		}
 	}
 
 	void FixedUpdate() {
